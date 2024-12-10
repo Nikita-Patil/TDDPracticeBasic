e5903e804be5805d31f675f787feffd9ac7799d3
Nikita-Patil <37011059+Nikita-Patil@users.noreply.github.com>
2024-12-10 23:50:19 +0530

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTestProject
+{
+	public class Fibonacci
+	{
+		public static int GetNthFibonacci(int n)
+		{
+			if (n <= 0)
+				throw new ArgumentException("N must be a positive integer.");
+
+			if (n == 1)
+				return 0;  
+			if (n == 2)
+				return 1;  
+
+			int a = 0, b = 1, c = 0;
+
+			for (int i = 3; i <= n; i++)
+			{
+				c = a + b;
+				a = b;
+				b = c;
+			}
+
+			return c; 
+		}
+	}
+}
